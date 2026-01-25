f11b91e89866816e70da4bc06526fa3c5ebc62dd
shin4488 <efshinya48@gmail.com>
2026-01-25 16:25:26 +0900

    Add Sentry gems and configuration

--- Gemfile
+++ Gemfile
@@ -39,7 +39,11 @@ gem "bootsnap", require: false
 gem 'rexml'
 gem 'rubyzip', require: 'zip'
 gem "figaro"
+
 gem 'lograge'
+gem "sentry-ruby"
+gem "sentry-rails"
+
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,11 @@
+
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+  config.traces_sample_rate = 1.0
+
+end
